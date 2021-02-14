d5d069c161c2b92d867d6e36a25f5034f76df8d6
HansBug <killog@126.com>
2021-02-14 23:55:39 +0800

    add some other tests

--- test/control/process/test_others.py
+++ test/control/process/test_others.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+import where
 
 from pji.control import common_process
 
@@ -36,6 +37,53 @@ class TestControlProcessOthers:
         _result = cp.result
         assert _result is not None
         assert _result.ok
+
+    @pytest.mark.timeout(5.0)
+    def test_dict_resource(self):
+        with common_process(
+                args="python3 -c \"while True: pass\"",
+                resources=dict(
+                    max_real_time='2s',
+                )
+        ) as cp:
+            pass
+
+        _result = cp.result
+        assert _result is not None
+        print(_result)
+
+    def test_invalid_resource(self):
+        with pytest.raises(TypeError):
+            with common_process(
+                    args="python3 -c \"while True: pass\"",
+                    resources=[1, 2, 3],
+            ):
+                pytest.fail('Should not reach here.')
+
+    def test_no_such_cwd(self):
+        with pytest.raises(FileNotFoundError):
+            with common_process(
+                    args='echo 233',
+                    cwd='/path/not/exist/wtf',
+            ):
+                pytest.fail('Should not reach here.')
+
+    def test_not_a_directory(self):
+        file = where.first('sh') or where.first('python') or where.first('python3')
+        with pytest.raises(NotADirectoryError):
+            with common_process(
+                    args='echo 233',
+                    cwd=file,
+            ):
+                pytest.fail('Should not reach here.')
+
+    def test_real_time_limit_key(self):
+        with pytest.raises(KeyError):
+            with common_process(
+                    args='echo 233',
+                    real_time_limit=4.0,
+            ):
+                pytest.fail('Should not reach here.')
 
 
 if __name__ == "__main__":
