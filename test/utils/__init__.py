c529260b142b7e95d24498d8c965d7312807f0ff
HansBug <killog@126.com>
2021-02-16 00:59:56 +0800

    add gen_lock

--- test/utils/__init__.py
+++ test/utils/__init__.py
@@ -1,4 +1,6 @@
 from .test_args import TestUtilsArgs
 from .test_decorator import TestUtilsDecorator
+from .test_iter import TestUtilsIter
+from .test_repr import TestUtilsRepr
 from .test_units import TestUtilsUnits
 from .test_value import TestUtilsValue
---
+++ test/utils/test_iter.py
@@ -0,0 +1,45 @@
+import os
+from threading import Thread
+
+import pytest
+
+from pji.utils import gen_lock
+
+
+@pytest.mark.unittest
+class TestUtilsIter:
+    def test_gen_lock_simple(self):
+        assert list(gen_lock([1, 2, 3, 4])) == [1, 2, 3, 4]
+
+    def test_gen_lock_func(self):
+        def _yield_func():
+            for i in range(1, 5):
+                yield i
+
+        assert list(gen_lock(_yield_func())) == [1, 2, 3, 4]
+
+    def test_gen_lock_threading(self):
+        _original = range(1, 1001, 1)
+        _generator = gen_lock(_original)
+
+        _list = []
+
+        def _yield_func():
+            for item in _generator:
+                _list.append(item)
+
+        t1 = Thread(target=_yield_func)
+        t2 = Thread(target=_yield_func)
+        t3 = Thread(target=_yield_func)
+
+        ts = [t1, t2, t3]
+        for t in ts:
+            t.start()
+        for t in ts:
+            t.join()
+
+        assert sorted(_list) == sorted(list(_original))
+
+
+if __name__ == "__main__":
+    pytest.main([os.path.abspath(__file__)])
