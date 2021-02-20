58a86e6c980f119fb1a769507a60f76d7e67891d
HansBug <killog@126.com>
2021-02-20 21:43:55 +0800

    add test for path.py

--- pji/utils/path.py
+++ pji/utils/path.py
@@ -9,8 +9,12 @@ def is_relative_path(path: str) -> bool:
     return not os.path.isabs(path)
 
 
-def is_inner_relative_path(path: str) -> bool:
+def is_inner_relative_path(path: str, allow_root: bool = True) -> bool:
     if is_relative_path(path):
-        return os.path.normpath(path).split(os.sep)[0] != '..'
+        segments = os.path.normpath(path).split(os.sep)
+        if not allow_root and segments == ['.']:
+            return False
+        else:
+            return segments[0] != '..'
     else:
         return False
--- test/utils/test_path.py
+++ test/utils/test_path.py
@@ -2,11 +2,30 @@ import os
 
 import pytest
 
+from pji.utils import is_absolute_path, is_relative_path, is_inner_relative_path
+
 
 @pytest.mark.unittest
 class TestUtilsPath:
     def test_is_absolute(self):
-        pass
+        assert is_absolute_path('/')
+        assert is_absolute_path('/root/1/2/3')
+        assert not is_absolute_path('.')
+        assert not is_absolute_path('../root/1/2/3')
+
+    def test_is_relative(self):
+        assert not is_relative_path('/')
+        assert not is_relative_path('/root/1/2/3')
+        assert is_relative_path('.')
+        assert is_relative_path('../root/1/2/3')
+
+    def test_is_inner_relative(self):
+        assert not is_inner_relative_path('/')
+        assert not is_inner_relative_path('/root/1/2/3')
+        assert is_inner_relative_path('.')
+        assert not is_inner_relative_path('.', allow_root=False)
+        assert not is_inner_relative_path('..')
+        assert not is_inner_relative_path('../1/..')
 
 
 if __name__ == "__main__":
