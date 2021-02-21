498fbebd86b971b9507afe1fe1e36572d99c3190
HansBug <killog@126.com>
2021-02-21 23:16:23 +0800

    move _check functions to base

--- pji/service/section/input/base.py
+++ pji/service/section/input/base.py
@@ -1,10 +1,39 @@
+import os
 from abc import ABCMeta, abstractmethod
 from typing import Optional
 
 from pysystem.models.authority.full import FileUserAuthority, FileAuthority, FileGroupAuthority, FileOtherAuthority
 
+from ....utils import is_inner_relative_path
 
-def _load_privilege_for_file_input(privilege=None) -> Optional[FileAuthority]:
+
+def _check_os_path(path: str) -> str:
+    """
+    check file valid or not, when valid, just process it
+    :param path: original file path
+    :return: normalized file path
+    """
+    return os.path.normpath(path)
+
+
+def _check_workdir_path(path: str) -> str:
+    """
+    check local path valid or not, when valid, just process it
+    :param path: original local path
+    :return: normalized local path
+    """
+    if not is_inner_relative_path(path, allow_root=False):
+        raise ValueError(
+            'Inner relative file path expected for local but {actual} found.'.format(actual=repr(path)))
+    return os.path.normpath(path)
+
+
+def _load_privilege(privilege=None) -> Optional[FileAuthority]:
+    """
+    load privilege information from data
+    :param privilege: raw privilege data
+    :return: privilege object or None
+    """
     if privilege is not None:
         try:
             _privilege = FileAuthority.loads(privilege)
--- pji/service/section/input/copy.py
+++ pji/service/section/input/copy.py
@@ -3,29 +3,8 @@ from typing import Optional, Mapping
 
 from pysystem import FileAuthority, chmod
 
-from .base import FileInputTemplate, FileInput, _load_privilege_for_file_input
-from ....utils import is_inner_relative_path, auto_copy_file, get_repr_info, env_template
-
-
-def _check_file(file: str) -> str:
-    """
-    check file valid or not, when valid, just process it
-    :param file: original file path
-    :return: normalized file path
-    """
-    return os.path.normpath(file)
-
-
-def _check_local(local: str) -> str:
-    """
-    check local path valid or not, when valid, just process it
-    :param local: original local path
-    :return: normalized local path
-    """
-    if not is_inner_relative_path(local, allow_root=False):
-        raise ValueError(
-            'Inner relative file path expected for local but {actual} found.'.format(actual=repr(local)))
-    return os.path.normpath(local)
+from .base import FileInputTemplate, FileInput, _load_privilege, _check_os_path, _check_workdir_path
+from ....utils import auto_copy_file, get_repr_info, env_template
 
 
 class _ICopyFileInput:
@@ -64,7 +43,7 @@ class CopyFileInputTemplate(FileInputTemplate, _ICopyFileInput):
         """
         self.__file = file
         self.__local = local
-        self.__privilege = _load_privilege_for_file_input(privilege)
+        self.__privilege = _load_privilege(privilege)
 
         _ICopyFileInput.__init__(self, self.__file, self.__local, self.__privilege)
 
@@ -89,8 +68,8 @@ class CopyFileInputTemplate(FileInputTemplate, _ICopyFileInput):
         :return: copy file input object
         """
         environ = environ or {}
-        _file = os.path.normpath(os.path.join(scriptdir, _check_file(env_template(self.__file, environ))))
-        _local = os.path.normpath(os.path.join(workdir, _check_local(env_template(self.__local, environ))))
+        _file = os.path.normpath(os.path.join(scriptdir, _check_os_path(env_template(self.__file, environ))))
+        _local = os.path.normpath(os.path.join(workdir, _check_workdir_path(env_template(self.__local, environ))))
 
         return CopyFileInput(
             file=_file, local=_local,
