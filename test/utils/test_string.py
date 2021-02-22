f747ed90aa2360b977a01082dedee526b8f044db
HansBug <killog@126.com>
2021-02-22 10:39:35 +0800

    add test for truncate

--- test/utils/test_string.py
+++ test/utils/test_string.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from pji.utils import env_template
+from pji.utils import env_template, truncate
 
 
 @pytest.mark.unittest
@@ -12,6 +12,13 @@ class TestUtilsString:
         assert env_template('this is the ${number} day.', dict(number='1st')) == 'this is the 1st day.'
         with pytest.raises(KeyError):
             env_template('this is the ${number} day')
+
+    def test_truncate(self):
+        assert truncate(
+            'this is the first time we do this kind of thing') == 'this is the first time we do this kind of thing'
+        assert truncate('this is the first time we do this kind of thing', width=30) == 'this is the first time we ... '
+        assert truncate('this is the first time we do this kind of thing', width=40, tail_length=12,
+                        show_length=True) == 'this is the ..(47 chars).. ind of thing'
 
 
 if __name__ == "__main__":
