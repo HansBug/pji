d2b4d4c99747f7f69a4a2d8d87ed1bd550f9e10e
HansBug <killog@126.com>
2021-02-23 17:49:51 +0800

    add self implement makedirsZ

--- pji/utils/__init__.py
+++ pji/utils/__init__.py
@@ -5,7 +5,7 @@ from .encoding import auto_decode, auto_encode, auto_decode_support, auto_encode
 from .file import FilePool, auto_copy_file, auto_link_file
 from .iter import gen_lock
 from .json import auto_load_json, JsonLoadError
-from .path import is_absolute_path, is_relative_path, is_inner_relative_path
+from .path import is_absolute_path, is_relative_path, is_inner_relative_path, makedirs
 from .repr import get_repr_info
 from .string import env_template, truncate
 from .units import size_to_bytes, time_to_duration, size_to_bytes_str, time_to_delta_str
--- test/utils/test_path.py
+++ test/utils/test_path.py
@@ -1,8 +1,11 @@
 import os
+import shutil
+import tempfile
 
 import pytest
+from pysystem import FileAuthority, SystemUser, SystemGroup
 
-from pji.utils import is_absolute_path, is_relative_path, is_inner_relative_path
+from pji.utils import is_absolute_path, is_relative_path, is_inner_relative_path, makedirs
 
 
 @pytest.mark.unittest
@@ -26,6 +29,29 @@ class TestUtilsPath:
         assert not is_inner_relative_path('.', allow_root=False)
         assert not is_inner_relative_path('..')
         assert not is_inner_relative_path('../1/..')
+
+    def test_makedirs(self):
+        with tempfile.TemporaryDirectory() as td:
+            _target_dir = os.path.join(td, '1', '2', '..', '3', '4')
+            makedirs(_target_dir, '356', 'nobody', 'nogroup')
+
+            _target_dir = os.path.normpath(_target_dir)
+            assert os.path.exists(_target_dir)
+            assert FileAuthority.load_from_file(_target_dir) == FileAuthority.loads('-wxr-xrw-')
+            assert SystemUser.load_from_file(_target_dir) == SystemUser.loads('nobody')
+            assert SystemGroup.load_from_file(_target_dir) == SystemGroup.loads('nogroup')
+            assert FileAuthority.load_from_file(os.path.join(_target_dir, '..')) == FileAuthority.loads('-wxr-xrw-')
+            assert SystemUser.load_from_file(os.path.join(_target_dir, '..')) == SystemUser.loads('nobody')
+            assert SystemGroup.load_from_file(os.path.join(_target_dir, '..')) == SystemGroup.loads('nogroup')
+            assert FileAuthority.load_from_file(os.path.join(_target_dir, '../..')) == FileAuthority.loads('-wxr-xrw-')
+            assert SystemUser.load_from_file(os.path.join(_target_dir, '../..')) == SystemUser.loads('nobody')
+            assert SystemGroup.load_from_file(os.path.join(_target_dir, '../..')) == SystemGroup.loads('nogroup')
+
+    def test_makedirs_invalid(self):
+        with tempfile.TemporaryDirectory() as td:
+            shutil.copyfile('README.md', os.path.join(td, 'r'))
+            with pytest.raises(NotADirectoryError):
+                makedirs(os.path.join(td, 'r', '1', '2'), '356', 'nobody', 'nogroup')
 
 
 if __name__ == "__main__":
