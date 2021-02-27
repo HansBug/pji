f2d63bc3ebfa647759294e2770596acf299f01f6
HansBug <killog@126.com>
2021-02-27 11:05:37 +0800

    update pji/__init__.py

--- pji/__init__.py
+++ pji/__init__.py
@@ -1,1 +1,3 @@
-from pji.control.process import *
+from .control import common_process, interactive_process, CommonProcess, InteractiveProcess, TimingContent, common_run, \
+    timing_run, mutual_run, ProcessResult, RunResult, RunResultStatus, ResourceLimit, Identification
+from .service import DispatchTemplate, Dispatch, TaskTemplate, Task
