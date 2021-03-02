aebc7e30504366f7cae0a1b169b9d683c2074c53
HansBug <killog@126.com>
2021-03-02 09:22:12 +0800

    add info dumps

---
+++ pji/entry/cli/result.py
@@ -0,0 +1,21 @@
+def _result_to_json(result):
+    _name, (_ok, _runs, _info) = result
+    return dict(
+        name=_name,
+        ok=_ok,
+        result=[item.json for item in _runs],
+        information=_info,
+    )
+
+
+def to_json(success: bool, result):
+    """
+    from result to json data
+    :param success: success or not
+    :param result: full result
+    :return: full json
+    """
+    return dict(
+        ok=not not success,
+        result=[_result_to_json(item) for item in result],
+    )
