6cfe422ee243e8c4f9bfc69c967248e65a6bcd2f
HansBug <hansbug@buaa.edu.cn>
2022-01-29 13:53:56 +0800

    dev(hansbug): remove auto_encode and auto_decode in local

--- pji/utils/__init__.py
+++ pji/utils/__init__.py
@@ -2,7 +2,7 @@ from .args import args_split
 from .collection import duplicates
 from .context import eclosing
 from .decorator import allow_none
-from .encoding import auto_decode, auto_encode, auto_decode_support, auto_encode_support
+from .encoding import auto_decode_support, auto_encode_support
 from .file import FilePool, auto_copy_file, auto_link_file
 from .func import wrap_empty
 from .iter import gen_lock
--- pji/utils/encoding.py
+++ pji/utils/encoding.py
@@ -1,29 +1,10 @@
 from typing import Union, Callable, TypeVar
 
-import chardet
-
-_ENCODING_LIST = ['utf-8', 'gbk', 'gb2312', 'gb18030', 'big5']
+from hbutils.encoding import auto_decode
 
 
-def auto_encode(string: str) -> bytes:
-    return string.encode()
-
-
-def auto_decode(data: Union[bytes, bytearray]) -> str:
-    auto_encoding = chardet.detect(data)['encoding']
-    if auto_encoding and auto_encoding not in _ENCODING_LIST:
-        _list = _ENCODING_LIST + [auto_encoding]
-    else:
-        _list = _ENCODING_LIST
-
-    last_err = None
-    for enc in _list:
-        try:
-            return data.decode(encoding=enc)
-        except UnicodeDecodeError as err:
-            last_err = err
-
-    raise last_err
+def _auto_encode(s: str) -> bytes:
+    return s.encode()
 
 
 _T = TypeVar('_T')
@@ -35,7 +16,7 @@ def auto_encode_support(func: Callable[[Union[bytes, bytearray], ], _T]) \
         if isinstance(data, (bytes, bytearray)):
             return func(data)
         elif isinstance(data, str):
-            return func(auto_encode(data))
+            return func(_auto_encode(data))
         else:
             raise TypeError("Unknown type to encode support - {cls}".format(cls=type(data).__class__))
 
