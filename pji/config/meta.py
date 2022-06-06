18d1d7fa6e99bb4052b58b1bbe2f6ffae83ab594
HansBug <hansbug@buaa.edu.cn>
2022-06-06 13:21:04 +0800

    release(hansbug): use version v0.2.2

--- pji/config/meta.py
+++ pji/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "pji"
 
 #: Version of this project.
-__VERSION__ = "0.2.1"
+__VERSION__ = "0.2.2"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Python library which can do complex test in educational area.'
